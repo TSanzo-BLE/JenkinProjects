3c180dadd231a0395f36b928370cdde03fa56d6c
Tonia <sanzo.tonia@gmail.com>
2021-11-05 19:28:21 -0400

    Create Run_Pipeline_From_Github.groovy

---
+++ Run_Pipeline_From_Github/Run_Pipeline_From_Github.groovy
@@ -0,0 +1,14 @@
+Pipeline
+{
+    agent any
+    stages
+    {
+        stage('Build_mock')
+        {
+
+            echo <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+            echo   'If I was building the engine this is where I would do it'
+            echo <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        }
+    }
+}
