388249e1b05fc633e437b047249e539c115b45f1
Tonia <sanzo.tonia@gmail.com>
2021-11-05 19:55:30 -0400

    Update Run_Pipeline_From_Github.groovy

--- Run_Pipeline_From_Github/Run_Pipeline_From_Github.groovy
+++ Run_Pipeline_From_Github/Run_Pipeline_From_Github.groovy
@@ -2,9 +2,11 @@ pipeline {
     agent any
     stages {
         stage('Build_mock') {
-            sh echo '--------------------------------------------------------------'
-            sh echo '   If I was building the engine this is where I would do it   '
-            sh echo '--------------------------------------------------------------'
+            steps {
+                sh echo '--------------------------------------------------------------'
+                sh echo '   If I was building the engine this is where I would do it   '
+                sh echo '--------------------------------------------------------------'
+            }
         }
     }
 }
