d237c4296d497bf0fbce408ab3c3ce2108abe434
Tonia <sanzo.tonia@gmail.com>
2021-11-05 19:49:53 -0400

    Unrecognized commands
    debugging pipeline

--- Run_Pipeline_From_Github/Run_Pipeline_From_Github.groovy
+++ Run_Pipeline_From_Github/Run_Pipeline_From_Github.groovy
@@ -1,15 +1,10 @@
-pipeline
-{
+pipeline {
     agent any
-    stages
-    {
-        stage('Build_mock')
-        {
-            echo
-            echo '--------------------------------------------------------------'
-            echo   'If I was building the engine this is where I would do it'
-            echo '-------------------------------------------------------------'
-            echo
+    stages {
+        stage('Build_mock') {
+            sh echo '--------------------------------------------------------------'
+            sh echo '   If I was building the engine this is where I would do it   '
+            sh echo '--------------------------------------------------------------'
         }
     }
 }
